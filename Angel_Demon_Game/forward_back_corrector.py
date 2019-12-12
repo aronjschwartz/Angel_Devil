0aea7833670d285bc583bbf65d54c51023c6bb5d
aronjschwartz <aschwa2@pdx.edu>
2019-12-11 16:38:53 -0800

    Added comments to correction code

--- Angel_Demon_Game/forward_back_corrector.py
+++ Angel_Demon_Game/forward_back_corrector.py
@@ -1,6 +1,6 @@
-#Program: Measures the y value of the lower line, outputs forward/back correction code
+#File: Measures the approximate forward/back adjustment needed using Canny Edge detection and hough line filtering
 
-
+#Package imports
 import numpy as np
 import math
 import os
@@ -17,13 +17,15 @@ def display_image(image):
 def read_image(image):
 	pic = cv2.imread(image, flags=cv2.IMREAD_COLOR)
 	return pic
-	
+
+#Function to find the intersection of two lines, obtained from:
 #https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
 def findIntersection(x1,y1,x2,y2,x3,y3,x4,y4):
 	px= ( (x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) ) 
 	py= ( (x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) )
 	return (int(px), int(py))
 
+#Function to return a forward/back correction code from an average y-value input
 def interpret_y_for_forward_back_code(y_val):
 	correction_code = 0
 	print("The avg y val is: ", y_val)
@@ -33,7 +35,7 @@ def interpret_y_for_forward_back_code(y_val):
 		correction_code = 1
 	return correction_code
 		
-
+#Function to obtain all lines within a slope threshold from a hough lines list
 def get_rho_theta_horizontals(hough_lines_list):
 	horizontals = []
 	thetas = []
@@ -49,7 +51,8 @@ def get_rho_theta_horizontals(hough_lines_list):
 				thetas.append(theta)
 				rhos.append(rho)
 	return horizontals, thetas, rhos
-	
+
+#Function to obtain the X, Y values of a line from polar coordinates
 def get_xy_values(rho, theta):
 	a = np.cos(theta)
 	b = np.sin(theta)
@@ -65,74 +68,114 @@ def get_xy_values(rho, theta):
 		slope = 0
 	return [x1, y1, x2, y2, slope]
 	
+#Primary function to process an image and obtain the approximate rotation offset
 def process_image():
-	
+	#Take a fresh capture
 	img = cv2.VideoCapture(0)
+	#Read into proper format
 	ret, img = img.read()
 	#Resize the image for consistency
-	img = cv2.resize(img, (1200, 960))
-	#Display original image
+	img = cv2.resize(img, (300, 240))
+	
 	#Crop the portion of the photo we care about
 	height = img.shape[0]
 	width = img.shape[1]
 	width_fourth = int(width/4)
 	
 	#y range, x range for cropping
-
-	lower_cropped = img[0:height, width_fourth:width - width_fourth]
+	upper_cropped = img[0:height, width_fourth:width - width_fourth]
 	
 	#Set filter bounds to all blue colors
-	lower_bound = np.array([100,0,0])
+	lower_bound = np.array([30,0,0])
 	upper_bound = np.array([225,80,80])
 	
-	blue_mask_lower = cv2.inRange(lower_cropped, lower_bound, upper_bound)
+	#Apply the blue mask to the cropped image
+	blue_mask_upper = cv2.inRange(upper_cropped, lower_bound, upper_bound)
 	
-	result_lower =  cv2.bitwise_and(lower_cropped, lower_cropped, mask=blue_mask_lower)
+	#Reduce non-blue pixels to be black, keep blue as blue
+	#Reduce the image to black and white
+	result_upper = np.stack((blue_mask_upper,blue_mask_upper,blue_mask_upper), axis=2)
 	
-	edges_lower = cv2.Canny(result_lower,25,150,apertureSize = 3)
-	minLineLength = 20
-	maxLineGap = 30
+	cv2.imwrite('masked_image' + '.jpg',result_upper)
 	
-	lines_lower = cv2.HoughLines(edges_lower,1,np.pi/180,20,minLineLength,maxLineGap)
-
+	#Apply canny edge detection to the result image
+	edges_upper = cv2.Canny(result_upper,25,150,apertureSize = 3)
 	
-	#Lower crop line parameters lists
-	horizontals_lower = []
-	verticals_lower = []
-	theta_list_lower = []
-	rho_list_lower = []
+	# Copy edges to the images that will display the results in BGR
+	cdst = cv2.cvtColor(edges_upper, cv2.COLOR_GRAY2BGR)
+	cv2.imwrite('canny_result' + '.jpg',cdst)
+	
+	cdstP = np.copy(cdst)
+	
+	#Hough line parameters
+	minLineLength = 500#20
+	maxLineGap = 0#30
+	
+	#Apply the hough lines transform to the canny results
+	lines_upper = cv2.HoughLines(edges_upper,
+								 5,   # rho, resolution of parameter r in pixels
+								 np.pi/180,
+								 70,   # threshold
+								 minLineLength,
+								 maxLineGap)
+	
+	#Extract information from lines list
+	if lines_upper is not None:
+		for i in range(0, len(lines_upper)):
+			rho = lines_upper[i][0][0]
+			theta = lines_upper[i][0][1]
+			a = math.cos(theta)
+			b = math.sin(theta)
+			x0 = a * rho
+			y0 = b * rho
+			pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+			pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+			cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+		cv2.imwrite('hough_result' + '.jpg',cdst)
+	
+	
+	#Upper crop line parameter lists
+	horizontals_upper = []
+	verticals_upper = []
+	theta_list_upper = []
+	rho_list_upper = []
+	
 	#Only analyze images with valid lines found, should be always
-	
+	if lines_upper is None:
+		return None
+	elif len(lines_upper) <= 1:
+		return None
+	else:
+		#Translate lines
+		upper_horizontals, upper_thetas, upper_rhos = get_rho_theta_horizontals(lines_upper, cdstP)
 		
-	if len(lines_lower) > 1:
-		lower_horizontals, lower_thetas, lower_rhos = get_rho_theta_horizontals(lines_lower)
-
 		angle_sum = 0.0
 		rho_sum = 0.0
 		#Get the average angle of the horizontal lines
-		for angle in lower_thetas:
+		for angle in upper_thetas:
 			angle_sum += angle
-		avg_theta = float(angle_sum/len(lower_thetas))
+		avg_theta = float(angle_sum/len(upper_thetas))
 		
 		#Get average length of lines
-		for val in lower_rhos:
+		for val in upper_rhos:
 			rho_sum += val
-			
-		avg_rho = float(rho_sum/len(lower_rhos))
+		
+		#Create the "average" line
+		avg_rho = float(rho_sum/len(upper_rhos))
 		avg_line_parameters = get_xy_values(avg_rho, avg_theta)
 		x1 = avg_line_parameters[0]
 		y1 = avg_line_parameters[1]
 		x2 = avg_line_parameters[2]
 		y2 = avg_line_parameters[3]
 		avg_y = float((y1 + y2)/2)
+		#Interpret average y for a correction code
 		forward_back_code = interpret_y_for_forward_back_code(avg_y)
 	
 		cv2.line(result_lower,(x1, y1),(x2, y2),(0,0,255),2)
 		cv2.imwrite('lower_result' + '.jpg',result_lower)
 		
 	else:
-		forward_back_code = "NONE"
-		
-
+		forward_back_code = None
+	#Return the correction code
 	return forward_back_code
 			
--- Angel_Demon_Game/rotation_corrector.py
+++ Angel_Demon_Game/rotation_corrector.py
@@ -1,6 +1,6 @@
-#Program: Measures the angle of the central blue grid line, calcuates the angle, and outputs a recommended "correction"
+#File: Measures the approximate angle of the forward grid line using combination of canny edge detection and hough line generation
 
-
+#Package imports
 import numpy as np
 import math
 import os
@@ -14,11 +14,10 @@ from scipy import ndimage
 # TODO: consider how to transform from 2d angle with in the image to 3d spacial rotation of robot
 # TODO: implement some way of sorting the slopes of the detected lines and searching for groupings, to determine outliers
 # TODO: implement some way of sorting the lines by their vertical height and building groups, to hopefully find two clusters and discard the top cluster
-
 # TODO: investigate how using a black-and-white image is handled differently by Canny than a blue-and-black image
 
 
-#Display image with cv
+#Display image with cv2 library
 def display_image(image):
 	cv2.imshow("TEST", image)
 	cv2.waitKey(0)
@@ -28,13 +27,15 @@ def read_image(image):
 	pic = cv2.imread(image, flags=cv2.IMREAD_COLOR)
 	return pic
 
+#Function to find the intersection of two lines, obtained from:
 #https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
 def findIntersection(x1,y1,x2,y2,x3,y3,x4,y4):
 	px= ( (x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) )
 	py= ( (x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) )
 	return (int(px), int(py))
 
-#This function takes an angle and outputs a recommend correction code
+#Function with ability to output correction code based on angle
+#Currently returns just the angle
 def interpret_angle_for_rotation_code(angle):
 	
 	return angle
@@ -53,25 +54,29 @@ def interpret_angle_for_rotation_code(angle):
 	#     correction_code = -3
 	# return correction_code
 
+#Function to obtain all lines within a slope threshold from a hough lines list
 def get_rho_theta_horizontals(hough_lines_list, img):
 	horizontals = []
 	thetas = []
 	rhos = []
+	#Loop through each line in the hough lines list
 	for line in hough_lines_list:
 		for rho, theta in line:
-			#Get the x,y,slope values from polar coordinates
+			#Get the x,y,slope values from polar coordinates for each rho, theta
 			x1, y1, x2, y2, slope = get_xy_values(rho, theta)
-			#Horizontal line
+			#Slope threshold to throw out blatantly vertical lines that could be accidentatlly picked up
 			slope_threshold = 0.4
+			#If we pass the slope threshold, append to output lists
 			if ((-slope_threshold < slope) and (slope < slope_threshold)):
 				cv2.line(img,(x1, y1),(x2, y2),(0,0,255),2)
-				print(slope)
 				horizontals.append(slope)
 				thetas.append(theta)
 				rhos.append(rho)
+	#Write to debug image
 	cv2.imwrite('lines_on_canny' + '.jpg',img)
 	return horizontals, thetas, rhos
 
+#Function to obtain the X, Y values of a line from polar coordinates
 def get_xy_values(rho, theta):
 	a = np.cos(theta)
 	b = np.sin(theta)
@@ -87,24 +92,22 @@ def get_xy_values(rho, theta):
 		slope = 9999
 	return [x1, y1, x2, y2, slope]
 
-
+#Primary function to process an image and obtain the approximate rotation offset
 def process_image():
-	
-	#Resize the image for consistency
+	#Take a fresh capture
 	img = cv2.VideoCapture(0)
+	#Read into proper format
 	ret, img = img.read()
+	#Resize the image for consistency
 	img = cv2.resize(img, (300, 240))
-	#img = cv2.resize(img, (1200, 960))
-	#Display original image
+	
 	#Crop the portion of the photo we care about
 	height = img.shape[0]
 	width = img.shape[1]
 	width_fourth = int(width/4)
 	
 	#y range, x range for cropping
-	#Top portion used for rotation adjustment
 	upper_cropped = img[0:height, width_fourth:width - width_fourth]
-	
 	
 	#Set filter bounds to all blue colors
 	lower_bound = np.array([30,0,0])
@@ -113,17 +116,14 @@ def process_image():
 	#Apply the blue mask to the cropped image
 	blue_mask_upper = cv2.inRange(upper_cropped, lower_bound, upper_bound)
 	
-	#reduce non-blue stuff to be black, keep blue as blue
-	# result_upper =  cv2.bitwise_and(upper_cropped, upper_cropped, mask=blue_mask_upper)
-	# reduce the image to black and white
+	#Reduce non-blue pixels to be black, keep blue as blue
+	#Reduce the image to black and white
 	result_upper = np.stack((blue_mask_upper,blue_mask_upper,blue_mask_upper), axis=2)
 	
 	cv2.imwrite('masked_image' + '.jpg',result_upper)
 	
-	
 	#Apply canny edge detection to the result image
 	edges_upper = cv2.Canny(result_upper,25,150,apertureSize = 3)
-	
 	
 	# Copy edges to the images that will display the results in BGR
 	cdst = cv2.cvtColor(edges_upper, cv2.COLOR_GRAY2BGR)
@@ -131,7 +131,7 @@ def process_image():
 	
 	cdstP = np.copy(cdst)
 	
-	# these parameters do nothing? maybe used in probabalistic transofrm but not normal version???
+	#Hough line parameters
 	minLineLength = 500#20
 	maxLineGap = 0#30
 	
@@ -143,7 +143,7 @@ def process_image():
 								 minLineLength,
 								 maxLineGap)
 	
-	
+	#Extract information from lines list
 	if lines_upper is not None:
 		for i in range(0, len(lines_upper)):
 			rho = lines_upper[i][0][0]
@@ -170,6 +170,7 @@ def process_image():
 	elif len(lines_upper) <= 1:
 		return None
 	else:
+		#Translate lines
 		upper_horizontals, upper_thetas, upper_rhos = get_rho_theta_horizontals(lines_upper, cdstP)
 		
 		angle_sum = 0.0
@@ -183,6 +184,7 @@ def process_image():
 		for val in upper_rhos:
 			rho_sum += val
 		
+		#Create the "average" line
 		avg_rho = float(rho_sum/len(upper_rhos))
 		avg_line_parameters = get_xy_values(avg_rho, avg_theta)
 		x1 = avg_line_parameters[0]
@@ -192,13 +194,12 @@ def process_image():
 		
 		#Calculate the angle with respect to horizontal for the average line
 		radians = math.atan2(y1-y2, x2-x1)
-		true_angle = math.degrees(radians)
-		# rotation_correction_code = interpret_angle_for_rotation_code(true_angle)
-		
+		true_angle = math.degrees(radians)	
+		#Draw the average line and write to image
 		cv2.line(result_upper,(x1, y1),(x2, y2),(0,0,255),2)
-		
 		cv2.imwrite('upper_result_final' + '.jpg',result_upper)
+		#Return the calculated angle of offset
 		return true_angle
 	
-	#print("File: ", str(file), " analysis: Rotation correction: ", str(rotation_correction_code), " Forward/Back code: ", forward_back_code)
+	
     
